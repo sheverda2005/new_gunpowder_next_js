ef5b6912fecdea0a8f2bd34f40c6cd553c2ca655
sheverda <nsewerda04@gmail.com>
2024-07-16 11:49:24 +0300

    seacrh google

---
+++ src/app/googlefe7eabb734a0b4ab/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <>
+            google-site-verification: googlefe7eabb734a0b4ab.html
+        </>
+    );
+};
+
+export default Page;
